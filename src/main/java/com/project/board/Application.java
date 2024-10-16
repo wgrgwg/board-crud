89f4bfc3028ce219d72a129ad6d51b18ed793475
wgrgwg <kyoeng14697@naver.com>
2024-10-16 13:15:37 +0900

    init commit

---
+++ src/main/java/com/project/board/Application.java
@@ -0,0 +1,15 @@
+package com.project.board;
+
+import java.util.Scanner;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("한글도 입력 : ");
+        String string = sc.nextLine();
+
+        System.out.println(string);
+    }
+}
