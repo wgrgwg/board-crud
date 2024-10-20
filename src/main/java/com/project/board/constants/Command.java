0c60e522a48ad18e429de9c75b9e0c118c42e170
wgrgwg <kyoeng14697@naver.com>
2024-10-20 18:17:59 +0900

    feat(constants): add Command enum class
    add Command enum class to handle command input from user.

---
+++ src/main/java/com/project/board/constants/Command.java
@@ -0,0 +1,19 @@
+package com.project.board.constants;
+
+public enum Command {
+    CREATE("작성"),
+    READ("조회"),
+    UPDATE("수정"),
+    DELETE("삭제"),
+    EXIT("종료");
+
+    private final String text;
+
+    Command(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+}
