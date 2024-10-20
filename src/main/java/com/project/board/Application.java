33400ed55ddc3ed76d158637d6952f9b001f6e98
wgrgwg <kyoeng14697@naver.com>
2024-10-20 21:15:05 +0900

    feat: add basic logic to run the program

--- src/main/java/com/project/board/Application.java
+++ src/main/java/com/project/board/Application.java
@@ -1,10 +1,15 @@
 package com.project.board;
 
+import com.project.board.controller.BoardController;
+import com.project.board.view.BoardView;
 import java.util.Scanner;
 
 public class Application {
 
     public static void main(String[] args) {
-        // TO DO
+        BoardView boardView = new BoardView();
+        BoardController boardController = new BoardController(boardView);
+
+        boardController.run();
     }
 }
