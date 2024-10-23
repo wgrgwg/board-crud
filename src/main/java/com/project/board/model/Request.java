7837e65564099ad4df64dd7cd0cd6bf8e35f9330
wgrgwg <kyoeng14697@naver.com>
2024-10-23 17:31:42 +0900

    feat(model: add Request class

--- src/main/java/com/project/board/model/Request.java
+++ src/main/java/com/project/board/model/Request.java
@@ -1,5 +1,17 @@
 package com.project.board.model;
 
+import com.project.board.constants.Feature;
+import com.project.board.constants.Type;
+import java.util.List;
+
 public class Request {
-    
+    private Type type;
+    private Feature feature;
+    List<Object> paramList;
+
+    public Request(Type type, Feature feature, List<Object> paramList) {
+        this.type = type;
+        this.feature = feature;
+        this.paramList = paramList;
+    }
 }
